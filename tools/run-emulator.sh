35d47e861e06faa934afb9f525a986b1aedff892
Denis Sermukhamedov <den.medov@gmail.com>
2016-11-14 03:18:38 +0600

    run 82 keyevent after test-butler apk installation

--- tools/run-emulator.sh
+++ tools/run-emulator.sh
@@ -7,7 +7,6 @@ emulator64-arm -avd test -no-audio -no-window &
 
 echo "waiting boot of emulator..."
 android-wait-for-emulator.sh
-
-adb shell input keyevent 82
+sleep 5
 adb install /opt/tools/test-butler-app-1.1.0.apk
-
+adb shell input keyevent 82
