793f5b4ec3726b615775505245ca1a11d6f9162b
Denis Sermukhamedov <den.medov@gmail.com>
2016-11-14 01:54:23 +0600

    add test-butler apk, and add laucn it after android emulator started

--- tools/run-emulator.sh
+++ tools/run-emulator.sh
@@ -8,5 +8,6 @@ emulator64-arm -avd test -no-audio -no-window &
 echo "waiting boot of emulator..."
 android-wait-for-emulator.sh
 
-
+adb shell input keyevent 82
+adb install test-butler-app-1.1.0.apk
 
