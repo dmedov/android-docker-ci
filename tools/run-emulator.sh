995d47bf99a8a8d1d0649f32cb0999f1ec7b12e1
Denis Sermukhamedov <den.medov@gmail.com>
2016-11-15 17:34:10 +0600

    set emulator settings via adb before tests

--- tools/run-emulator.sh
+++ tools/run-emulator.sh
@@ -7,10 +7,13 @@ echo "no" | emulator64-arm -avd test -no-audio -no-window &
 
 echo "waiting boot of emulator..."
 android-wait-for-emulator.sh
-adb shell input keyevent 82 &
-sleep 4
-adb install /opt/tools/test-butler-app-1.1.0.apk
+adb shell input keyevent 82
+adb shell svc power stayon true
+adb shell settings put global window_animation_scale 0
+adb shell settings put global transition_animation_scale 0
+adb shell settings put global animator_duration_scale 0
 
+adb install /opt/tools/test-butler-app-1.1.0.apk
 
 adb logcat & 
 
